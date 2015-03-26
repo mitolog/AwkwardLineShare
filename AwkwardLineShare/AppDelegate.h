a06c1a238af9c38dd1717e2fdda303ea73126ffe
mito <hkyhm5l@gmail.com>
2015-03-27 02:41:22 +0900

    Initial Commit

---
+++ AwkwardLineShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AwkwardLineShare
+//
+//  Created by YuheiMiyazato on 3/27/15.
+//  Copyright (c) 2015 mitolab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AwkwardLineShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AwkwardLineShare
+//
+//  Created by YuheiMiyazato on 3/27/15.
+//  Copyright (c) 2015 mitolab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
